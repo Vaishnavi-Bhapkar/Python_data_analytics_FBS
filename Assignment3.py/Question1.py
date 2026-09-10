697be2c21de1d9154b60eea324df86cbaef119bb
Vaishnavi Bhapkar <vaishnavibhapkar143@gmail.com>
2026-09-10 22:48:13 +0530

    Delete Assignment3.py directory

--- Assignment3.py/Question1.py
+++
@@ -1,9 +0,0 @@
-        ## Check if the number is positive or negative
-
-
-number = int (input('enter number :'))
-
-if number > 0 :
-    print('positive:')
-else:
-    print('negetive')
--- Assignment3.py/Question7.py
+++
@@ -1,9 +0,0 @@
-           ## user entered correct userid and password...
-
-userid =int(input ("enter userid: "))
-password =int(input("enter password: "))
-
-if userid == 1234 and password == 1234:
-    print("user enter correct userid and password")
-else:
-    print("user enter incorrect ")
